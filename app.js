ffb05d1eb97cc6699a1e07d2941237b772dcddad
abdulhaseeb2112172 <bscs2112172@szabist.pk>
2025-06-28 23:52:20 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,9 @@
 const express = require('express');
-const { Pool } = require('pg');
 const app = express();
 const port = 3000;
 
-const pool = new Pool({
-  user: 'postgres',
-  host: 'db-container',
-  database: 'postgres',
-  password: 'secret',
-  port: 5432,
-});
-
-app.use(express.json());
-
 app.get('/', (req, res) => {
-  res.send('Task Manager Home');
-});
-
-app.post('/task', async (req, res) => {
-  const { title } = req.body;
-  try {
-    await pool.query('INSERT INTO tasks (title) VALUES ($1)', [title]);
-    res.status(201).send('Task created');
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Error inserting task');
-  }
+  res.send("Hello from Haseeb's Task Manager App");
 });
 
 app.listen(port, () => {
